fbbd1d24487bf59f679e38779f94cf0051741818
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-06 17:09:27 -0600

    se agrego el menu y las librerias <conio.h> y <cstdlib>

--- main.cpp
+++ main.cpp
@@ -2,10 +2,44 @@
 #include<conio.h>
 #include<cstdlib>
 
+#include "laboratorio.h"
+
 using namespace std;
 
 int main(){
 
+    Laboratorio l;
+    string op;
+
+    while (true){
+        
+        cout << "1) Agregar personaje" << endl;
+        cout << "2) Mostrar" << endl;
+        cout << "3) Respaldar" << endl;
+        cout << "4) Recuperar" << endl;
+        cout << "5) Salir" << endl;
+        getline(cin, op);
+
+        if (op == "1"){
+            
+        }
+        else if (op == "2"){
+            l.mostrar();
+        }
+        else if (op == "3"){
+            l.respaldar();
+        }
+        else if (op == "4"){
+            l.recuperar();
+        }
+        else if (op == "5"){
+            break;
+        }
+        
+        
+        
+
+    }
     
 
 
