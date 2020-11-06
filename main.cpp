30aa026cd2d32478391a81963e3820ef1ff30103
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-06 17:33:34 -0600

    se agrego la funcionalidad del cada opcion del menu

--- main.cpp
+++ main.cpp
@@ -13,18 +13,31 @@ int main(){
 
     while (true){
         
+        system("cls");
         cout << "1) Agregar personaje" << endl;
         cout << "2) Mostrar" << endl;
         cout << "3) Respaldar" << endl;
         cout << "4) Recuperar" << endl;
         cout << "5) Salir" << endl;
+        cout << "Opcion:" << endl;
         getline(cin, op);
 
+
+
+
         if (op == "1"){
-            
+            Computadora c;
+
+            cin >> c;
+
+            l.agregarPersonaje(c);
+            cin.ignore();
         }
         else if (op == "2"){
             l.mostrar();
+            cout << endl;
+            system("pause");
+
         }
         else if (op == "3"){
             l.respaldar();
@@ -33,12 +46,9 @@ int main(){
             l.recuperar();
         }
         else if (op == "5"){
+            system("pause");
             break;
         }
-        
-        
-        
-
     }
     
 
