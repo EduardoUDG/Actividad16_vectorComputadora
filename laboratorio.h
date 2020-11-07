e09d8ad6e4813cbf9c9e9dd2d69fbeab876d7980
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-06 18:51:52 -0600

    se crea la implementacion de buscar

--- laboratorio.h
+++ laboratorio.h
@@ -21,6 +21,7 @@ public:
     void inicializar(const Computadora &c, size_t n);
     void eliminar(size_t pos);
     void ordenar();
+    Computadora* buscar(const Computadora &c);
 
     friend Laboratorio& operator<<(Laboratorio &v, const Computadora &p)
     {
